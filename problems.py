e28f0bc9150b44422aa2135e20d183fee1c5f912
fixpt671-coder <kosnyrevf@yandex.ru>
2024-09-06 12:06:02 +0300

    1st HW

---
+++ problems.py
@@ -0,0 +1,22 @@
+# 1st program
+print((9**0.5)*5)
+
+# 2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# 3rd program
+print(2*2 + 2)
+print(2*(2 + 2))
+print(2*2 + 2 == 2*(2 + 2))
+
+# 4th.1 program
+sstr = "123.456"
+print(sstr[4])
+
+# 4th.2 program
+sstr = "123.456"
+number = float(sstr)
+number *= 10
+number = int(number)
+number %= 10
+print(number)
