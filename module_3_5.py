90046455fb0bd017e14870e666e028fdb41daafc
fixpt671-coder <kosnyrevf@yandex.ru>
2024-10-11 00:58:45 +0300

    Add files via upload

--- module_3_5.py
+++ module_3_5.py
@@ -1,6 +1,8 @@
 def multiplied_digits(number):
     str_number = str(number)
     first_digit = int(str_number[0])
+    if first_digit == 0:
+        first_digit = 1
     if len(str_number) > 1:
         return first_digit * multiplied_digits(int(str_number[1:]))
     return first_digit
@@ -8,8 +10,10 @@ def multiplied_digits(number):
 def multiplied_digits_2(number):
     last_digit = number%10
     next_number = number//10
+    if last_digit == 0:
+        last_digit = 1
     if next_number > 0:
         return last_digit*multiplied_digits(next_number)
     return 1
 
-print(multiplied_digits(123456789), multiplied_digits_2(321456987))
+print(multiplied_digits(30), multiplied_digits_2(30))
